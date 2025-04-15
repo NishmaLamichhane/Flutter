9d7745c87ccb231e931ab655fb0a375d8afeb606
NishmaLamichhane <nishmalamichhane2005@gmail.com>
2025-04-15 21:07:09 +0545

    Simple Calculator

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class Calculator extends StatefulWidget {
   const Calculator({super.key});
@@ -37,72 +38,112 @@ class _CalculatorState extends State<Calculator> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
-        title: const Text(
-          "Let's make an calculator to find addition, subtraction, multiplication and division",
-        ),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: firstNumberController,
-              decoration: InputDecoration(
-                labelText: 'Enter first Number',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                  borderSide: const BorderSide(color: Colors.blue, width: 3.0),
+      // appBar: AppBar(
+      //   backgroundColor: Colors.blueAccent,
+      //   title: const Text(
+      //     "Let's make an calculator to find addition, subtraction, multiplication and division",
+      //   ),
+      //   centerTitle: true,
+      // ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.only(
+            top: 50,
+            left: 20,
+            right: 20,
+            bottom: 20,
+          ),
+          color: Colors.blue[100],
+          width: 400,
+          height: 600,
+
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                Text(
+                  "Simple Calculator",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
                 ),
-                floatingLabelAlignment: FloatingLabelAlignment.start,
-                floatingLabelBehavior: FloatingLabelBehavior.never,
-              ),
-            ),
-            SizedBox(height: 16),
-            TextField(
-              controller: secondNumberController,
-              decoration: InputDecoration(
-                labelText: 'Enter second Number',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                  borderSide: const BorderSide(color: Colors.blue, width: 3.0),
+                SizedBox(height: 10),
+                TextField(
+                  controller: firstNumberController,
+                  decoration: InputDecoration(
+                    labelText: 'Enter first Number',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide: const BorderSide(
+                        color: Colors.blue,
+                        width: 3.0,
+                      ),
+                    ),
+                    floatingLabelAlignment: FloatingLabelAlignment.start,
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                  ),
                 ),
-                floatingLabelAlignment: FloatingLabelAlignment.start,
-                floatingLabelBehavior: FloatingLabelBehavior.never,
-              ),
+                SizedBox(height: 16),
+                TextField(
+                  controller: secondNumberController,
+                  decoration: InputDecoration(
+                    labelText: 'Enter second Number',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide: const BorderSide(
+                        color: Colors.blue,
+                        width: 3.0,
+                      ),
+                    ),
+                    floatingLabelAlignment: FloatingLabelAlignment.start,
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () => _calculate('add'),
+                  child: const Text('ADD'),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () => _calculate('subtract'),
+                  child: const Text('SUBTRACT'),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () => _calculate('multiply'),
+                  child: const Text('MULTIPLY'),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () => _calculate('divide'),
+                  child: const Text('DIVIDE'),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'Result:',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'The result is $result',
+                  style: const TextStyle(fontSize: 30, color: Colors.black),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    firstNumberController.clear();
+                    secondNumberController.clear();
+                    setState(() {
+                      result = 0;
+                    });
+                  },
+                  child: const Text('Clear'),
+                ),
+              ],
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => _calculate('add'),
-              child: const Text('ADD'),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () => _calculate('subtract'),
-              child: const Text('SUBTRACT'),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () => _calculate('multiply'),
-              child: const Text('MULTIPLY'),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () => _calculate('divide'),
-              child: const Text('DIVIDE'),
-            ),
-            const SizedBox(height: 20),
-            Text(
-              'Result:',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              'The result is $result',
-              style: const TextStyle(fontSize: 30, color: Colors.black),
-            ),
-          ],
+          ),
         ),
       ),
     );
